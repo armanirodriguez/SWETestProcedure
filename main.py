0a1d3e5fc2fbe26f6f541a08067a5b85d586b408
Armani Rodriguez <armanilrodriguez@gmail.com>
2022-02-07 22:26:09 -0500

    Added procedure table and sample data

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+# example test data
+data = [
+#   id  name            version      approved   
+    [1, "Login",        1.0,        True],
+    [2, "Registration", 1.0,        False],
+    [3, "Payment",      1.0,        True]
+]
+
+
+
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title="Home Page")
 
 @app.route('/procedures')
 def procedures():
-    return render_template("procedures.html")
+    return render_template("procedures.html", title="Procedures", data=data)
 
 app.run(debug=True)
