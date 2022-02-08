28cc527f329839bc5ad18f47b35c5c2e6deacaba
Armani Rodriguez <armanilrodriguez@gmail.com>
2022-02-07 21:58:45 -0500

    added templates and an additional route

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>hello world!</h1>"
+    return render_template("home.html")
+
+@app.route('/procedures')
+def procedures():
+    return render_template("procedures.html")
 
 app.run(debug=True)
