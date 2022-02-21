04e9a9550f1fb849d1d1f7a1471d9ef56276d5b0
Dew <andrewhehls@gmail.com>
2022-02-21 12:53:29 -0500

    fixed models merge

--- models.py
+++ models.py
@@ -3,7 +3,6 @@ from app import app,db
 
 class TestProcedure(db.Model):
     __tablename__ = 'testprocedure'
-<<<<<<< HEAD
     pk_prodID = db.Column(db.Integer, primary_key=True)
     createdBy = db.Column(db.String, nullable=False)
     dateCreated = db.Column(db.String, nullable=False)
@@ -12,10 +11,6 @@ class TestProcedure(db.Model):
     status = db.Column(db.String(), nullable=False)
     priority = db.Column(db.String, nullable=False)
     estTimeToComplete = db.Column(db.String(), nullable=False)
-=======
-    pk_procedure = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
->>>>>>> bdce5f82078f97d1febc5c6d9bf6adb73b7f0b82
     version = db.Column(db.String(5), nullable=False)
     approval = db.Column(db.Boolean, nullable=False)
     notes = db.Column(db.Text, nullable=True)
@@ -33,17 +28,12 @@ class TestProcedure(db.Model):
         self.approval = approval
         self.notes = notes
         
+        
 class TestStep(db.Model):
     __tablename__ = 'teststep'
-<<<<<<< HEAD
     pk_stepID = db.Column(db.Integer, primary_key=True)
     testStepName = db.Column(db.Text, nullable=False) # actual test step
     fk_prodID = db.Column(db.Integer, db.ForeignKey('testprocedure.pk_prodID'))
-=======
-    pk_step = db.Column(db.Integer, primary_key=True)
-    test_step = db.Column(db.Text, nullable=False) # actual test step
-    fk_TestStep_TestProcedure = db.Column(db.Integer, db.ForeignKey('testprocedure.id'))
->>>>>>> bdce5f82078f97d1febc5c6d9bf6adb73b7f0b82
     procedure = db.relationship('TestProcedure', backref="testprocedure")
     def __init__(self, test_step, procedure_fk_id):
         self.test_step = test_step
