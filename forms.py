f7f012941a36725bd153555d0a54769e5be2d420
Dew <andrewhehls@gmail.com>
2022-02-21 12:48:54 -0500

    added editing procedures

--- forms.py
+++ forms.py
@@ -1,12 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, PasswordField, SubmitField, BooleanField
+from wtforms import IntegerField, StringField, SelectField, SubmitField, BooleanField, DateTimeField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.widgets import TextArea
+from wtforms.fields import DateField
 
 class procedureForm(FlaskForm):
-    name = StringField('Name',
+    formCreatedBy = StringField('Created By',
 							validators=[DataRequired(), Length(min=2, max=20)])
-    version = StringField('Version', validators=[DataRequired(), Length(min=3, max=3)])
-    approval = BooleanField('Approval')
-    notes = StringField('Notes')
+    formDateCreated = StringField('Date Created', )
+    formProdName = StringField('Name',
+							validators=[DataRequired(), Length(min=2, max=20)])
+    formRequirements = StringField('Requirements', widget=TextArea())
+    formStatus = StringField('Status', validators=[DataRequired()])
+    formPriority = StringField('Priority') #
+    formEstTime = StringField('Est Time', validators=[DataRequired()])
+    formVersion = StringField('Version', validators=[DataRequired(), Length(min=3, max=3)])
+    formApproval = BooleanField('Approval')
+    formNotes = StringField('Notes', widget=TextArea())
     submit = SubmitField('Save')
     cancel = SubmitField('Cancel')
+
+    
--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from forms import procedureForm
 from flask_sqlalchemy import SQLAlchemy
 from models import TestProcedure
 from app import db
-
-
-
-
-
-
 from app import app
 
 @app.route("/")
@@ -19,7 +13,6 @@ def home():
 @app.route("/procedures")
 def procedures():
     procedures = TestProcedure.query.all()
-    print(procedures)
     return render_template("procedures.html", title="Procedure", dataTable=procedures)
 
 @app.route("/new_procedure", methods=['GET', 'POST'])
@@ -27,12 +20,36 @@ def new_procedure():
     form = procedureForm()
     # When form is submitted, create a 
     if form.validate_on_submit():
-        myData = TestProcedure(form.name.data, form.version.data, form.approval.data, form.notes.data)
+        myData = TestProcedure(form.formCreatedBy.data, form.formDateCreated.data, form.formProdName.data, form.formRequirements.data, form.formStatus.data, form.formPriority.data,
+                                form.formEstTime.data, form.formVersion.data, form.formApproval.data, form.formNotes.data)
         db.session.add(myData)
         db.session.commit()
-        flash(f'Procedure created for {form.name.data}!', 'success')
+        flash(f'Procedure created for {form.formProdName.data}!', 'success')
         return redirect(url_for('procedures'))
     return render_template("new_procedure.html", title="New Procedure", form=form)
+
+@app.route("/edit/<int:id>", methods=['GET', 'POST'])
+def edit(id):
+    editForm = procedureForm()
+    editProd = TestProcedure.query.get_or_404(id)
+    editForm.formApproval.checked = editProd.approval
+    if editForm.validate_on_submit():
+        editProd.createdBy = editForm.formCreatedBy.data
+        editProd.dateCreated = editForm.formDateCreated.data
+        editProd.prodName = editForm.formProdName.data
+        editProd.requirements = editForm.formRequirements.data
+        editProd.status = editForm.formStatus.data
+        editProd.priority = editForm.formPriority.data
+        editProd.estTimeToComplete = editForm.formEstTime.data
+        editProd.version = editForm.formVersion.data
+        editProd.approval = editForm.formApproval.data
+        editProd.notes = editForm.formNotes.data
+
+        db.session.commit()
+        flash(f'Procedure {editForm.formProdName.data} updated!', 'dark')
+        return redirect(url_for('procedures'))
+    return render_template("edit.html", title="Edit Procedure", editForm=editForm, editProd=editProd)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
