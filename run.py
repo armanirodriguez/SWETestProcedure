aa8d7875e1c9445ffe8a8a38321e84cd7050d744
Armani Rodriguez <armanilrodriguez@gmail.com>
2022-03-01 15:33:53 -0500

    removed unused imports

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 from app import app
-import argparse
-
-parser = argparse.ArgumentParser(description='Run the Test Management Web Application')
 
 if __name__ == '__main__':
     app.run(debug=True)
