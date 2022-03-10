44cd72b79127555d1c40deaea873bc559040ecef
joshfigueroa <jrfigueroa1228@gmail.com>
2022-03-09 21:12:57 -0500

    Added requirements for unit tests Added basic unit test template

---
+++ tests/__init__.py
@@ -0,0 +1,18 @@
+import pytest
+from app import app
+from tests import test_db
+
+@pytest.fixture
+def client():
+    """Configures the app for testing
+
+    Sets app config variable ``TESTING`` to ``True``
+
+    :return: App for testing
+    """
+
+    app.config['TESTING'] = True
+    
+    client = app.test_client()
+
+    yield client
