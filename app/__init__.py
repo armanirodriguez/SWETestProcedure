fba6cc18adfdd314ba2654cbfa9bab8a81d0d639
Armani Rodriguez <armanilrodriguez@gmail.com>
2022-03-31 17:31:01 -0400

    added login

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
-# Create a flask application, and load the database within the app folder. If no databae exists, create one. Config app and database keys#
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -10,6 +10,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 
 from app import routes  # noqa: F401, E402
